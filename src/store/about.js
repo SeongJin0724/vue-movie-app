c320449b8363e5e9c7f752a30cac7a7788f6032c
SeongJin0724 <thls702464@gmail.com>
2022-01-24 15:10:11 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+    namespaced: true,
+    state: () => ({
+        name: 'SeongJin',
+        email: 'thls702464@gmail.com',
+        git: 'https://github.com/SeongJin0724',
+        phone: '+82-10-1234-5678',
+        image: 'https://heropy.blog/css/images/logo.png'
+    })
+}
